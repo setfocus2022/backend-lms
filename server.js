5eec84570d924fd9e303e28473597371436e37fa
matheusmiguel-santos <149403481+conectfam@users.noreply.github.com>
2023-10-30 16:35:17 -0300

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const pool = new Pool({
 });
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://fair-ruby-caterpillar-wig.cyclic.app', 'https://psico-painel.vercel.app'],
+  origin: ['http://localhost:3000', 'https://hungry-fawn-bandanna.cyclic.app', 'https://psico-painel-ten.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
