f3d8bb53e91caf9fba983c78240a4e7cbb62b27e
conectfam <149403481+conectfam@users.noreply.github.com>
2024-03-03 18:13:39 -0300

    Update server.js

--- server.js
+++ server.js
@@ -45,8 +45,9 @@ app.get('/api/generate-pdf/:username', async (req, res) => {
 
   // Adicionar o logo
   const logoPath = path.join(__dirname, 'images', 'logo2.png');
-  doc.image(logoPath, doc.page.width / 2 - 50, 50, { width: 300 }).fillColor('#FFF');
+  doc.image(logoPath, doc.page.width / 2 - 150, 50, { width: 300 });
 
+  // Move o texto para baixo para acomodar o logo maior
   doc.moveDown(3);
 
   // Adicionar conteúdo ao PDF
