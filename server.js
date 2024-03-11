5a55f5086bacc1f34ea14751efe5c07346f6acef
conectfam <149403481+conectfam@users.noreply.github.com>
2024-03-11 09:58:05 -0300

    Update server.js

--- server.js
+++ server.js
@@ -103,16 +103,16 @@ app.get('/api/certificado-concluido/:username/:cursoId', async (req, res) => {
   // Ajusta as coordenadas para inserir o nome do usuário e do curso
   // Observação: As coordenadas Y são medidas de baixo para cima no pdf-lib
   firstPage.drawText(userData.nome, {
-    x:   1205.5 , // Converta mm para pontos se necessário
-    y: height - 665.0 , // Converta mm para pontos e ajuste a partir da base
+    x:   1005.5 , // Converta mm para pontos se necessário
+    y: height - 555.0 , // Converta mm para pontos e ajuste a partir da base
     size: fontSize,
     font: font,
     color: rgb(0, 0, 0),
   });
 
   firstPage.drawText(cursoData.nome, {
-    x:  1205.5 , // Converta mm para pontos se necessário
-    y: height - 465.0 , // Ajuste Y conforme necessário
+    x:  1005.5 , // Converta mm para pontos se necessário
+    y: height - 765.0 , // Ajuste Y conforme necessário
     size: fontSize,
     font: font,
     color: rgb(0, 0, 0),
