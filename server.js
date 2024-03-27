8a7f9866d68ff3621007c8ac5937d2792a3fe5c6
conectfam <149403481+conectfam@users.noreply.github.com>
2024-03-27 13:28:33 -0300

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const pool = new Pool({
 });
 
 app.use(cors({
-  origin: ['http://localhost:3000','https://b9ff-187-109-132-62.ngrok-free.app' ,'https://precious-pike-coveralls.cyclic.app', 'https://connect-ead.vercel.app' , 'https://www.connectfam.com.br'],
+  origin: ['http://localhost:3000','https://b9ff-187-109-132-62.ngrok-free.app' ,'https://quaint-tank-top.cyclic.app/', 'https://connect-ead.vercel.app' , 'https://www.connectfam.com.br'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
